53bd65e560cd0604f55889ca8251c1bdabf70892
Zikri Ramdani <zikri_ramdani@bridgenote.id>
2024-05-07 22:32:15 +0700

    meta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,19 +7,48 @@
   <title>Tukang Listrik Panggilan - Jasa Perbaikan Listrik | Mekanik Elektro</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta content="Mekanik Elektro" property="og:title" />
-  <meta content="Mekanik Elektro ialah Teknisi Listrik / Tukang Listrik Panggilan yg melayani jasa perbaikan Listrik untuk Rumah, Ruko, Kantor, &amp; Industri." property="og:description" />
-  <meta content="" property="og:image" />
-  <meta content="MekanikElektro.com" property="og:url" />
+  <!-- Manifest -->
+  <link rel="manifest" href="http://mekanikelektro.com/manifest.json" />
+
+  <!-- Keyword & Author -->
+  <meta name="keywords" content="Mekanik Elektro, Teknisi Listrik, Tukang Listrik, Jakarta, Bogor, Depok, Bekasi, Tangerang" />
+  <meta name="author" content="ZRDevelopers" />
+
+  <!--  Essential META Tags -->
+  <meta property="og:title" content="Mekanik Elektro">
+  <meta property="og:type" content="article" />
+  <meta property="og:image" content="http://MekanikElektro.com/assets/assets/images/about-us.jpg">
+  <meta property="og:url" content="http://MekanikElektro.com">
+  <meta name="twitter:card" content="summary_large_image">
+
+  <!--  Non-Essential, But Recommended -->
+  <meta property="og:description" content="Mekanik Elektro ialah Teknisi Listrik / Tukang Listrik Panggilan yg melayani jasa perbaikan Listrik untuk Rumah, Ruko, Kantor, &amp; Industri.">
+  <meta property="og:site_name" content="Mekanik Elektro">
+  <meta name="twitter:image:alt" content="Mekanik Elektro">
+
+  <!--  Non-Essential, But Required for Analytics -->
+  <!-- <meta property="fb:app_id" content="your_app_id" />
+  <meta name="twitter:site" content="@website-username"> -->
 
   <!-- Favicon -->
   <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
 
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-WHCCSGXMFH"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-WHCCSGXMFH');
+  </script>
+
   @viteReactRefresh
 
   @vite("resources/app/index.jsx")
-
-
 </head>
 
 <body>
