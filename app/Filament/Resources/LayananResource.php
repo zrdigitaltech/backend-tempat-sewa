57c0644ec455a12fd506f9d8c9575c8c0a1d29f1
bn-zikriramdani <zikri_ramdani@bridgenote.id>
2024-06-14 08:24:38 +0700

    CRUD: Services

--- app/Filament/Resources/LayananResource.php
+++ app/Filament/Resources/LayananResource.php
@@ -12,6 +12,13 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Card;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextArea;
 
 class LayananResource extends Resource
 {
@@ -26,14 +33,25 @@ class LayananResource extends Resource
     protected static ?int $navigationSort = 2;
 
     protected static ?string $label = 'Services';
- 
+
     protected static ?string $slug = 'services';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+              Card::make()
+              ->schema([
+                  FileUpload::make('image')
+                      ->required(),
+                  TextInput::make('title')
+                      ->required()
+                      ->maxLength(255),
+                  TextInput::make('alt')
+                      ->maxLength(255),
+                  TextArea::make('description')
+                      ->maxLength(255),
+              ])->columnSpanFull()
             ]);
     }
 
@@ -41,17 +59,22 @@ class LayananResource extends Resource
     {
         return $table
             ->columns([
-                //
+              ImageColumn::make('image'),
+              TextColumn::make('title'),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\CreateAction::make()
+                    ->createAnother(false)
                 ]),
             ]);
     }
--- app/Filament/Resources/LayananResource/Pages/CreateLayanan.php
+++ app/Filament/Resources/LayananResource/Pages/CreateLayanan.php
@@ -9,4 +9,6 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateLayanan extends CreateRecord
 {
     protected static string $resource = LayananResource::class;
+
+    protected static bool $canCreateAnother = false;
 }
