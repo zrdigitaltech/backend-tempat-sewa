540311fab8879b605771d25acad07af51668e405
bn-zikriramdani <zikri_ramdani@bridgenote.id>
2024-06-19 14:41:01 +0700

    onprogress api

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\AreaLayananController;
 use App\Http\Controllers\Api\BannerController;
+use App\Http\Controllers\Api\CallToActionController;
 use App\Http\Controllers\Api\FloatingWhatsappController;
+use App\Http\Controllers\Api\GaleriController;
+use App\Http\Controllers\Api\KontakKamiController;
+use App\Http\Controllers\Api\LayananController;
 use App\Http\Controllers\Api\LogoController;
 use App\Http\Controllers\Api\NumberLayananController;
+use App\Http\Controllers\Api\PembayaranController;
+use App\Http\Controllers\Api\TentangKamiController;
+use App\Http\Controllers\Api\TestimoniController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +34,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('api')->group(function () {
   Route::get('/service-area', [AreaLayananController::class, 'index']);
   Route::get('/banner', [BannerController::class, 'index']);
+  Route::get('/call-to-action', [CallToActionController::class, 'index']);
   Route::get('/floating-whatsapp', [FloatingWhatsappController::class, 'index']);
+  Route::get('/galery', [GaleriController::class, 'index']);
+  Route::get('/contact-us', [KontakKamiController::class, 'index']);
+  Route::get('/services', [LayananController::class, 'index']);
   Route::get('/logos', [LogoController::class, 'index']);
   Route::get('/service-number', [NumberLayananController::class, 'index']);
+  Route::get('/payment', [PembayaranController::class, 'index']);
+  Route::get('/about-us', [TentangKamiController::class, 'index']);
+  Route::get('/testimonial', [TestimoniController::class, 'index']);
 });
