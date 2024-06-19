def09b66905020f8336487a50f0d08cf92d7b67c
bn-zikriramdani <zikri_ramdani@bridgenote.id>
2024-06-19 14:28:51 +0700

    route api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\AreaLayananController;
+use App\Http\Controllers\Api\BannerController;
+use App\Http\Controllers\Api\FloatingWhatsappController;
 use App\Http\Controllers\Api\LogoController;
+use App\Http\Controllers\Api\NumberLayananController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
 });
 
-Route::get('/logos', [LogoController::class, 'index']);
+Route::prefix('api')->group(function () {
+  Route::get('/service-area', [AreaLayananController::class, 'index']);
+  Route::get('/banner', [BannerController::class, 'index']);
+  Route::get('/floating-whatsapp', [FloatingWhatsappController::class, 'index']);
+  Route::get('/logos', [LogoController::class, 'index']);
+  Route::get('/service-number', [NumberLayananController::class, 'index']);
+});
