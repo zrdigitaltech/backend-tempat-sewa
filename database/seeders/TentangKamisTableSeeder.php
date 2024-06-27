bd30a233b143f31047ad1f2b02ebebd225614173
bn-zikriramdani <zikri_ramdani@bridgenote.id>
2024-06-27 10:30:53 +0700

    create class seed

---
+++ database/seeders/TentangKamisTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TentangKamisTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
