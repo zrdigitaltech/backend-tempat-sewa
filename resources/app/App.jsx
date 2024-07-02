3d324624e113688038908f4ee3358608991f6dca
bn-zikriramdani <zikri_ramdani@bridgenote.id>
2024-07-03 05:14:14 +0700

    clean

--- resources/app/App.jsx
+++ resources/app/App.jsx
@@ -1,24 +1,3 @@
-// import React, { Fragment, useEffect } from "react";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./router";
-
-// import AOS from "aos";
-// import "aos/dist/aos.css"; // You can also use <link> for styles
-
-// function App() {
-//     useEffect(() => {
-//         AOS.init();
-//     });
-
-//     return (
-//         <Fragment>
-//             <RouterProvider router={router}></RouterProvider>
-//         </Fragment>
-//     );
-// }
-
-// export default App;
-
 import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import store from '@/redux/store';
