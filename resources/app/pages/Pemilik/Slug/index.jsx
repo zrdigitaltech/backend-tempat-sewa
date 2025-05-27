f3632146b1932bb6affb7c64f16c8b56eeda6187
zikriramdani <zikriramdani@gmail.com>
2025-05-27 15:31:43 +0700

    Prog: Redesign UI

--- resources/app/pages/Pemilik/Slug/index.jsx
+++ resources/app/pages/Pemilik/Slug/index.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Breadcrumb from '@/app/components/Breadcrumb';
+import Heads from '@/app/components/Heads';
 import {
   PemilikProfileCard,
   SidebarDesktop,
@@ -69,6 +70,11 @@ const PemilikSlugPage = () => {
 
   return (
     <Fragment>
+      <Heads
+        title={`${pemilikProfile?.name} - ${pemilikProfile?.bio}`}
+        deskripsi={pemilikProfile?.bio}
+        image={pemilikProfile?.avatar}
+      />
       <div className="pb-5">
         <section className="mt-3">
           <Breadcrumb title={`Penulis: ${pemilikProfile?.name || slug}`} />
