5559143428f20e243cba06c83a1aa599d5b2edac
zikriramdani <zikriramdani@gmail.com>
2025-06-11 13:32:14 +0700

    Fix: Heading elements are not in a sequentially-descending order

--- resources/app/pages/Home/components/TipeProperti/index.jsx
+++ resources/app/pages/Home/components/TipeProperti/index.jsx
@@ -127,9 +127,9 @@ export default function Index() {
                     className=" text-dark"
                   >
                     <div className="card text-center border-0 shadow-sm h-100">
-                      <div className="card-body py-4">
+                      <div className="card-body ST-card-body py-4">
                         <div className="fs-2 mb-2">{iconTipeProperti(cat?.nama)}</div>
-                        <h6 className="card-title mb-0 text-truncate">Sewa {cat.nama}</h6>
+                        <h3 className="card-title mb-0 text-truncate">Sewa {cat.nama}</h3>
                       </div>
                     </div>
                   </Link>
