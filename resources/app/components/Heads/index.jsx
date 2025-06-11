3fd8ded562d74ea8d7e92833922c4e921c679222
zikriramdani <zikriramdani@gmail.com>
2025-06-11 15:38:58 +0700

    Add: SEO

--- resources/app/components/Heads/index.jsx
+++ resources/app/components/Heads/index.jsx
@@ -1,58 +1,79 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
-const Index = props => {
-  const { title, deskripsi, image } = props;
-
+const Index = ({ title, deskripsi, image, url }) => {
   const defaultTitle =
     'TempatSewa.Com Indonesia: Situs Sewa Kos, Sewa Rumah, Sewa Apartemen, Sewa Ruko, Sewa Kios dan Sewa Gudang';
   const defaultDeskripsi =
     'Temukan dan sewa kontrakan, kost, atau properti impianmu dengan mudah. Kelola dan pasarkan properti dalam satu platform: tempatSewa.Com.';
-  const defaultImage = image || '/assets/assets/images/about-us.jpg';
+  const defaultImage = image || 'https://tempatsewa.com/assets/images/about-us.jpg';
+  const currentUrl =
+    url || (typeof window !== 'undefined' ? window.location.href : 'https://tempatsewa.com');
+  const metaTitle = title || defaultTitle;
+  const metaDesc = deskripsi || defaultDeskripsi;
+
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: metaTitle,
+    url: currentUrl,
+    description: metaDesc,
+    publisher: {
+      '@type': 'Organization',
+      name: 'TempatSewa.Com',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://tempatsewa.com/assets/images/logo.png'
+      }
+    },
+    image: {
+      '@type': 'ImageObject',
+      url: defaultImage,
+      height: 630,
+      width: 1200
+    }
+  };
 
   return (
     <Helmet>
-      {/* <meta charSet="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
-
-      {/* Primary Meta Tags */}
-      {/* <title>{title || defaultTitle}</title>
-      <meta name="description" content={deskripsi || defaultDeskripsi} />
+      {/* General Meta */}
+      <title>{metaTitle}</title>
+      <meta name="description" content={metaDesc} />
       <meta
         name="keywords"
         content="sewa kontrakan, sewa kost, sewa rumah, cari kontrakan murah, kost bulanan, sewa apartemen, kontrakan Jakarta, kost dekat kampus, pasang iklan properti, platform sewa properti, properti disewakan, cari rumah sewa, kontrakan eksklusif, manajemen properti, tempat sewa terpercaya"
       />
       <meta name="author" content="ZRDevelopers" />
       <meta name="language" content="id" />
-      <meta name="copyright" content="TempatSewa.Com" />
       <meta name="robots" content="index, follow" />
-      <meta name="distribution" content="global" />
-      <meta name="rating" content="general" />
-      <meta name="revisit-after" content="7 days" />
-      <meta name="theme-color" content="#128C7E" /> */}
+      <meta name="theme-color" content="#128C7E" />
+      <link rel="canonical" href={currentUrl} />
 
-      {/* Open Graph / Facebook */}
-      {/* <meta property="og:type" content="website" />
-      <meta
-        property="og:url"
-        content={typeof window !== 'undefined' ? window.location.href : 'https://tempatsewa.com'}
-      />
-      <meta property="og:title" content={title || defaultTitle} />
-      <meta property="og:description" content={deskripsi || defaultDeskripsi} />
+      {/* Open Graph */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={currentUrl} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDesc} />
       <meta property="og:image" content={defaultImage} />
-      <meta property="og:site_name" content="TempatSewa.Com" /> */}
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:site_name" content="TempatSewa.Com" />
+      <meta property="og:locale" content="id_ID" />
 
-      {/* Twitter */}
-      {/* <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title || defaultTitle} />
-      <meta name="twitter:description" content={deskripsi || defaultDeskripsi} />
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:description" content={metaDesc} />
       <meta name="twitter:image" content={defaultImage} />
-      <meta name="twitter:image:alt" content={title || defaultTitle} /> */}
+      <meta name="twitter:image:alt" content={metaTitle} />
 
       {/* Favicon & Manifest */}
-      {/* <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.png" />
-      <link rel="manifest" href="/manifest.json" /> */}
+      <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon" />
+      <link rel="manifest" href="/manifest.json" />
+
+      {/* JSON-LD Structured Data */}
+      <script type="application/ld+json">{JSON.stringify(jsonLd)}</script>
     </Helmet>
   );
 };
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,11 +9,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    {{-- Default SEO --}}
+    {{-- Dynamic SEO --}}
     @php
         $title = $meta['title'] ?? 'TempatSewa.Com Indonesia: Situs Sewa Kos, Sewa Rumah, Sewa Apartemen, Sewa Ruko, Sewa Kios dan Sewa Gudang';
         $description = $meta['description'] ?? 'Temukan dan sewa kontrakan, kost, atau properti impianmu dengan mudah. Kelola dan pasarkan properti dalam satu platform: tempatSewa.Com.';
-        $image = $meta['image'] ?? '/assets/assets/images/about-us.jpg';
+        $image = $meta['image'] ?? asset('assets/images/about-us.jpg');
+        $url = url()->current();
     @endphp
 
     <title>{{ $title }}</title>
@@ -21,31 +22,54 @@
     <meta name="keywords" content="sewa kontrakan, sewa kost, sewa rumah, cari kontrakan murah, kost bulanan, sewa apartemen, kontrakan Jakarta, kost dekat kampus, pasang iklan properti, platform sewa properti, properti disewakan, cari rumah sewa, kontrakan eksklusif, manajemen properti, tempat sewa terpercaya" />
     <meta name="author" content="ZRDevelopers" />
     <meta name="language" content="id" />
-    <meta name="copyright" content="TempatSewa.Com" />
     <meta name="robots" content="index, follow" />
-    <meta name="distribution" content="global" />
-    <meta name="rating" content="general" />
-    <meta name="revisit-after" content="7 days" />
     <meta name="theme-color" content="#128C7E" />
+    <link rel="canonical" href="{{ $url }}" />
 
     {{-- Open Graph --}}
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="{{ url()->current() }}" />
+    <meta property="og:url" content="{{ $url }}" />
     <meta property="og:title" content="{{ $title }}" />
     <meta property="og:description" content="{{ $description }}" />
     <meta property="og:image" content="{{ $image }}" />
     <meta property="og:site_name" content="TempatSewa.Com" />
+    <meta property="og:locale" content="id_ID" />
 
-    {{-- Twitter --}}
+    {{-- Twitter Card --}}
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="{{ $title }}" />
     <meta name="twitter:description" content="{{ $description }}" />
     <meta name="twitter:image" content="{{ $image }}" />
     <meta name="twitter:image:alt" content="{{ $title }}" />
 
-    {{-- Favicon --}}
-    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.png" />
+    {{-- Favicon & Manifest --}}
+    <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon" />
     <link rel="manifest" href="/manifest.json" />
+
+    {{-- Optional JSON-LD Structured Data --}}
+    <script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "{{ $title }}",
+        "url": "{{ $url }}",
+        "description": "{{ $description }}",
+        "publisher": {
+          "@type": "Organization",
+          "name": "TempatSewa.Com",
+          "logo": {
+            "@type": "ImageObject",
+            "url": "{{ asset('assets/images/logo.png') }}"
+          }
+        },
+        "image": {
+          "@type": "ImageObject",
+          "url": "{{ $image }}",
+          "width": 1200,
+          "height": 630
+        }
+      }
+    </script>
   </head>
 
   <body>
@@ -53,13 +77,9 @@
       @livewire('database-notifications')
     @endif
 
-    <!-- Google Tag Manager (noscript) -->
-    <!-- End Google Tag Manager (noscript) -->
-
     <div id="app"></div>
-    <!-- class="h-100" -->
 
     @filamentScripts
-    @vite('resources/js/app.js')
+    @vite('resources/js/app.js') {{-- Jika ada JS tambahan non-React --}}
   </body>
 </html>
