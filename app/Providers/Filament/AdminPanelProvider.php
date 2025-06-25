1b1f7cfee4a4fe3e41313056b281751a8977ba30
zikriramdani <zikriramdani@gmail.com>
2025-06-25 17:34:09 +0700

    Deploy

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -115,8 +115,7 @@ class AdminPanelProvider extends PanelProvider
         ],
         isPersistent: true
       )
-      ->authMiddleware([Authenticate::class], isPersistent: true)
-      ->userCanAccess(fn (\App\Models\User $user) => true);
+      ->authMiddleware([Authenticate::class], isPersistent: true);
   }
 
   protected function getWidgetsForPermissions(): array
